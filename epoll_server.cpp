853f08314e79bd0f609183def8938a2367e72065
onemonomo <401637852@qq.com>
2020-06-11 00:10:02 +0800

    修改epoll_server

--- epoll_server.cpp
+++ epoll_server.cpp
@@ -12,8 +12,14 @@
 #include <thread>
 #include <iostream>
 
+// 感谢下列博客作者
 // https://blog.csdn.net/u012206617/article/details/89301624
-int EpollServer::Access()
+// https://blog.csdn.net/xiajun07061225/article/details/9250579
+
+EpollServer::EpollServer(short port, int queue) : AbstractServer(port, queue) {}
+EpollServer::~EpollServer() {}
+
+int EpollServer::Accept()
 {
     _epollfd = epoll_create(DFT_EPOLL_SIZE);
     epoll_event single;
@@ -28,23 +34,59 @@ int EpollServer::Access()
     int currentEventNum = 1;
     while(true) {
         int num = epoll_wait(_epollfd, _events, currentEventNum, 0); // 修改时延
-        switch(num) {
-            case(-1) :
-                perror("epoll wait.");
-                return -2;
-            case(0) :
-                printf("no connection.\n"); // 会不会疯狂输出？
-                break;
-            default :
-                for (int i = 0; i < num; ++i) {
-                    if (_events[i].data.fd == _listenfd) {
-                        continue; // TODO
-                    } else {
-                        Working();
-                    }
-                }
+        if (num == -1) {
+            perror("epoll wait.");
+            return -2;
+        }
+        if (num == 0) {
+            printf("no connection.\n"); // 会不会疯狂输出？
+            continue;
+        }
+        for (int i = 0; i < num; ++i) {
+            if (_events[i].data.fd == _listenfd) {
+                HandleNewConnection(_listenfd);
+            } else {
+                if (_events[i].data & EPOLLIN) // 接收数据
+                    HandleEpollIn(_events[i].data.fd);
+            }
         }
         // 怎么退出
     }
     // 析构close _epollfd;
 }
+
+void EpollServer::HandleNewConnection(int fd)
+{
+    int connfd = accept(listenfd,(sockaddr *)&clientaddr, &clilen);
+    epoll_event single;
+    single.data.fd = connfd;
+    single.events = EPOLLIN|EPOLLET;
+    epoll_ctl(_epollfd, EPOLL_CTL_ADD, connfd, &single);
+}
+
+void EpollServer::HandleEpollIn(int fd)
+{
+    if (fd < 0) return;
+
+    char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
+    int len = recv(fd, buffer, sizeof(buffer), 0);
+    printf("Client[fd:%u]:%s", fd, buffer);
+    if (strcmp(buffer, "exit\n") == 0) {
+        char end[] = "Good Bye.\n";
+        send(fd, end, sizeof(end), 0);
+        printf("Client request closing connection.\n");
+
+        epoll_event single;
+        single.data.fd = fd;
+        single.events = EPOLLIN|EPOLLET;
+        epoll_ctl(_epollfd, EPOLL_CTL_DEL, fd,&single);
+        close(fd);
+        return;
+    }
+    char ans[] = "ToDo.\n";
+    send(fd, ans, sizeof(ans), 0);
+    // 需不需要重新epoll_ctl mod?
+}
+
+void EpollServer::Working(int fd) {}
--- epoll_server.h
+++ epoll_server.h
@@ -4,14 +4,17 @@
 #include "abstract_server.h"
 
 const int DFT_EPOLL_SIZE = 20;
-class EpollServer : public MyServer
+class EpollServer : public AbstractServer
 {
 public:
-    EpollServer();
+    EpollServer(short port = DFT_PORT, int queue = DFT_QUEUE_NUM);
     ~virtual EpollServer();
     int Accept() override;
 private:
     void Working(int fd) override;
+    void HandleNewConnection(int fd);
+    void HandleEpollIn(int fd);
+    void HandleEpollOut(int fd);
     int _epollfd;
     epoll_event _events[DFT_EPOLL_SIZE];
 }
